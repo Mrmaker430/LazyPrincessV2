8d7ad4d6c003c7883943d6839362ddfce510a05d
Mrmaker430 <167637714+Mrmaker430@users.noreply.github.com>
2025-02-11 10:32:49 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
 
-    url = f'https://shrinkme.in/shortLink'
+    url = f'shrinkme.in'
     params = {'token': URL_SHORTNER_WEBSITE_API,
               'link': link,
               'format': 'json'
@@ -397,8 +397,8 @@ async def get_shortlink(link):
                     return data['shortlink']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://shrinkme.in/st?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'shrinkme.in/st?api={URL_SHORTNER_WEBSITE_API}&link={link}'
 
     except Exception as e:
         logger.error(e)
-        return f'https://shrinkme.in/st?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+        return f'shrinkme.in/st?api={URL_SHORTNER_WEBSITE_API}&link={link}'
