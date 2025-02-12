97f7856c150b145014de7cc7ac03f0cd38bde0bf
Mrmaker430 <167637714+Mrmaker430@users.noreply.github.com>
2025-02-12 11:57:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://envs.sh/7N.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1773034985').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1773034985,6952526124').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002097884361').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1773034985').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
